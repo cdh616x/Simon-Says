2f82a66014f1f7ea9b31a7d907d8919733b3afa6
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-10-09 12:22:42 -0400

    Add files via upload

---
+++ ffffff.py
@@ -0,0 +1,10 @@
+user_score = 0
+simon_pattern = input()
+user_pattern  = input()
+
+for i in range(len(simon_pattern)):
+    if user_pattern[i] == simon_pattern[i]:
+        user_score += 1
+    
+
+print('User score:', user_score)
